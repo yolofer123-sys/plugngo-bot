76815c714db9429e8f3cbad007f8c80b1f1d8eb8
yolofer123-sys <yolofer123@gmail.com>
2026-06-29 13:54:36 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -101,5 +101,5 @@ async function hacerPeticionWA(data) {
 }
 
 // Para que Vercel inicie la app
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server corriendo en puerto ${PORT}`));
+// Exportamos la app para que Vercel la pueda ejecutar
+module.exports = app;
